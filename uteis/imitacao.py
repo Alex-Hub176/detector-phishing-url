ce9526f7a8b18a2c6985eaf8e776612a1307bbe6
unknown <alexsousacosta78@gmail.com>
2026-03-31 17:34:39 -0300

    feat: adicionado verificador de imitacao ele verifica se tem marcas famosas no site, caracteres suspeitos no site e palavras suspeitas e marcas no site.

--- uteis/imitacao.py
+++ uteis/imitacao.py
@@ -1,1 +1,31 @@
+import json
+from urllib.parse import urlparse
 
+
+def imitacao(link):
+    with open("dados/imitacao_marcas.json", "r") as arquivo:
+        marcas = json.load(arquivo)
+    
+    with open("dados/palavras_perigosas.json", "r") as f:
+        palavras_perigosas = json.load(f)
+
+
+    site = urlparse(link).netloc.lower()
+    caracteres_exemplo = ["1","0","@","3"]
+
+    score = 0
+    for marca in marcas:
+        if marca in site:
+            score += 1      
+
+    for categoria, palavras in palavras_perigosas.items():
+        for p in palavras:
+            if marca and p in site:
+                score += 5
+
+    for caracteres in caracteres_exemplo:
+        if caracteres in site:
+            score += 4
+            
+    return score    
+        
